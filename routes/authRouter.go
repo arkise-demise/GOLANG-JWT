70fd4e5b53c889f7f81f413c435b641b3c26f548
Arkise Demise <117926187+arkise-demise@users.noreply.github.com>
2024-05-26 15:24:02 +0300

    Add files via upload

---
+++ routes/authRouter.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	controller "GOLANG-JWT/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("users/login", controller.Login())
+}
---
+++ routes/userRouter.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	controller "GOLANG-JWT/controllers"
+	"GOLANG-JWT/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
